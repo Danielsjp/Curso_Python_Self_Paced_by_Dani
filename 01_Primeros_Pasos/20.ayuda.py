79bd53216d0b40bf1e3fa31a7a75ff315c17ea8a
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-18 18:40:10 +0200

    se agrega 20.ayuda

---
+++ 01_Primeros_Pasos/20.ayuda.py
@@ -0,0 +1,8 @@
+#python dispone de un sistema de ayuda
+#podemos usar la función help() para obtener información sobre un objeto, clase o módulo
+#por ejemplo, si queremos obtener información sobre la clase str, podemos hacer lo siguiente:
+help(str)
+#otra forma es a traves de python -m pydoc str ejecutando este comando en la terminal, nos mostrará la documentación de la clase str.
+#python -m pydoc str
+#tb podemos arrancar un servidor web con la documentación de python, para ello ejecutamos el siguiente comando en la terminal:
+#python -m pydoc -b
