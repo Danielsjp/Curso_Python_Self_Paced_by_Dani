af80d35a8b355787e8b3eea7445b07274a11f387
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-18 17:19:09 +0200

    updates

---
+++ 01_Primeros_Pasos/18-constantes.py
@@ -0,0 +1,5 @@
+RUTA_ARCHIVO = "C:/Users/Usuario/Documents/archivo.txt"
+IVA = 0.21
+IVA_REDUCIDO = 0.10
+IVA_SUPER_REDUCIDO = 0.04
+#el uso de constantes en python es una buena practica de programacion, ya que nos permite definir valores que no deben cambiar a lo largo del programa, y nos ayuda a mejorar la legibilidad del codigo.
