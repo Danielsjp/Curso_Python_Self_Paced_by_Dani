1e2a768204af281e45ccdca2f0abcde47fd95f41
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-18 17:12:28 +0200

    updates

---
+++ 17-variables_buenas_practicas.py
@@ -0,0 +1,21 @@
+#esto son nombres validos de variables en python, que siguen las buenas practicas de nombrado de variables.
+nombre_usuario = "Juan"
+NombreUsuario = "Juan"
+nombre_1 = "Juan"
+NOMBRE_USUARIO = "Juan" #esto es una constante, que por convencion se escribe en mayusculas.
+_nombre_usuario = "Juan" #esto es una variable privada, que por convencion se escribe con un guion bajo al inicio.
+#estos son validos pero no siguen las buenas practicas de nombrado de variables.
+español = "Juan" #no se recomienda utilizar caracteres especiales en los nombres de variables.
+número = 10 #no se recomienda utilizar caracteres especiales en los nombres de variables.
+#estos no son validos y no siguen las buenas practicas de nombrado de variables.
+1nombre = "Juan" #no se recomienda comenzar los nombres de variables con un numero.
+nombre usuario = "Juan" #no se recomienda utilizar espacios en los nombres de variables.
+nombre-usuario = "Juan" #no se recomienda utilizar guiones en los nombres de variables.
+del = "Juan" #no se recomienda utilizar palabras reservadas del lenguaje como nombres de variables.
+#se pueden asiganar valores a varias variables al mismo tiempo, pero no es recomendable ya que puede dificultar la lectura del codigo.
+nombre, apellido, edad = "Juan", "Perez", 30
+#se recomiendo asignar valores a las variables de manera individual, para mejorar la legibilidad del codigo.
+nombre = "Juan"
+apellido = "Perez"
+edad = 30
+
