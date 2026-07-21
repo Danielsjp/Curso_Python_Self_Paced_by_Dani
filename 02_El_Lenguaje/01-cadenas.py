56d3ee1a67ba1e1dd69d1c871cd9369ec5ae0de8
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-21 09:31:54 +0200

    updates

---
+++ 02_El_Lenguaje/01-cadenas.py
@@ -0,0 +1,21 @@
+cadena= "Esto es una cadena de texto"
+print(cadena)
+cadena = "Esto" "es" "una" "cadena" "de" "texto"
+print(cadena)
+cadena = "Esto es una 'cadena de texto' con comillas simples"
+print(cadena)
+cadena = "Esto es una \"cadena de texto\" con comillas dobles usando el caracter de escape"
+print(cadena)
+cadena = 'Esto es una cadena de varias lineas \
+abarcando varias lineas de texto \
+y permiter que el texto se vea mas ordenado'
+print(cadena)
+cadena = "usando salto de linea \npara separar el texto en varias lineas"
+print(cadena)
+cadena = """Esto es una cadena de varias lineas
+abarcando varias lineas de texto
+y permite que el texto se vea mas ordenado"""
+print(cadena)
+print("hay muchos otros caracteres de escape, como por ejemplo: \t para tabulador, \r para retorno de carro, \b para retroceso, entre otros") #esto no ha funcionado en 
+# la terminal de windows, pero si en la de linux.
+print("Tambien existen caracteres unicode, como por ejemplo: \u00A9 para el simbolo de copyright, \u00AE para el simbolo de marca registrada, entre otros")
