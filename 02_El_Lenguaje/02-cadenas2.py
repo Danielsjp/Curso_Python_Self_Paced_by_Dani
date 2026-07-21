d66faba0a44ab1f627b90515983dce900acd531c
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-21 09:47:43 +0200

    1

---
+++ 02_El_Lenguaje/02-cadenas2.py
@@ -0,0 +1,6 @@
+edad = 4
+#cadena "Ivan tiene " + edad + " años" #esto no funciona, ya que no se puede concatenar un string con un entero, por lo que hay que convertir el entero a string usando la funcion str()
+cadena = "Ivan tiene " + str(edad) + " años" #esto si funciona
+print(cadena)
+#Podemos usar el asterisco para multiplicar
+print("Batman "*2 + "na"*5)
