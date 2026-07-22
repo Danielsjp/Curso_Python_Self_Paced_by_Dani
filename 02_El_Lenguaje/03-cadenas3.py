185d92d4769ad1be820c95c72eebe93ab814f81a
Daniel Sánchez Jiménez-Pajarero <daniel.sanchezj@local.eurecat.org>
2026-07-22 15:00:12 +0200

    update

---
+++ 02_El_Lenguaje/03-cadenas3.py
@@ -0,0 +1,8 @@
+#metodo format
+cadena = "Hola, me llamo {} y tengo {} años"
+print(cadena.format("Ivan", 25))
+cadena = "Hola, me llamo {} y tengo {} años"
+resultado = cadena.format("Ivan", 25)
+print(resultado)
+#el beneficio de guardar el resultado de la operacion en una variable es que podemos usarlo en varias ocasiones sin tener que repetir la operacion
+#Podemos usar el metodo format() para formatear cadenas de texto de varias maneras, como por ejemplo usando el metodo format() o f-strings, que veremos en los siguientes ejemplos.
